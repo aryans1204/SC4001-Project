bf8a48ee4e8af3f6aacf749e4c39e7129d4427d2
teamoteh <lim1473@gmail.com>
2023-10-31 00:16:04 +0800

    codes for aryan

--- data.py
+++ data.py
@@ -9,8 +9,9 @@ from PIL import Image
 import pytorch_lightning as pl
 
 DATA_DIRS = {
-    "adience": "adience/gender",
-    "celeba": "C:/Users/arsha/SC4001-Project/celeba",
+    
+    "adience": "C:/Users/teamo/Downloads/data/adience/gender",
+    "celeba": "C:/Users/teamo/Downloads/data/celeba",
 }
 
 GENDERS = {0: "Female", 1: "Male"}
@@ -61,25 +62,120 @@ class ImageDataset(Dataset):
 
         return img, img_label
 
-
+'''
 class AdienceDataset(ImageDataset):
     def __init__(self, root_dir, train=True, train_ratio=0.8, transform=None):
         super().__init__(transform)
 
-        for root, _dirs, files in os.walk(root_dir):
-            test_start = int(train_ratio * len(files))
-            img_files = files[test_start:]
+        # Iterate through both 'm' and 'f' subdirectories
+        for gender_dir in ['m', 'f']:
+            gender_path = os.path.join(root_dir, gender_dir)
 
-            if train:
-                img_files = files[:test_start]
+            for root, _, files in os.walk(gender_path):
+                test_start = int(train_ratio * len(files))
+                img_files = files[test_start:]
 
-            for img_file in img_files:
-                img_path = os.path.join(root, img_file)
-                img_label = GENDER_VALUES[os.path.split(root)[-1]]
+                if train:
+                    img_files = files[:test_start]
 
-                self.data.append(img_path)
-                self.labels.append(img_label)
+                for img_file in img_files:
+                    img_path = os.path.join(root, img_file)
+                    img_label = GENDER_VALUES[gender_dir] # use gender_dir instead of splitting root
 
+                    self.data.append(img_path)
+                    self.labels.append(img_label)
+'''
+
+class AdienceDataset(ImageDataset):
+    def __init__(self, root_dir, partition="train", train_ratio=0.8, transform=None):
+        super().__init__(transform)
+
+        self.partition = partition
+
+        # Create a function to partition the files based on the train_ratio
+        def split_files(files, ratio):
+            split_idx = int(ratio * len(files))
+            if partition == "train":
+                return files[:split_idx]
+            elif partition == "val":
+                # For this example, we'll use the rest of the train_ratio for validation. Adjust as needed.
+                return files[split_idx:split_idx + int((1 - ratio) * len(files) * 0.5)]
+            elif partition == "test":
+                return files[split_idx + int((1 - ratio) * len(files) * 0.5):]
+            else:
+                raise ValueError("Invalid partition")
+
+        # Iterate through both 'm' and 'f' subdirectories
+        for gender_dir in ['m', 'f']:
+            gender_path = os.path.join(root_dir, gender_dir)
+
+            for root, _, files in os.walk(gender_path):
+                img_files = split_files(files, train_ratio)
+
+                for img_file in img_files:
+                    img_path = os.path.join(root, img_file)
+                    img_label = GENDER_VALUES[gender_dir]  # use gender_dir instead of splitting root
+
+                    self.data.append(img_path)
+                    self.labels.append(img_label)
+
+adience_train_dataset = AdienceDataset(
+    DATA_DIRS["adience"],
+    partition="train",
+    transform=DATA_TRANSFORMS["train"],
+)
+adience_test_dataset = AdienceDataset(
+    DATA_DIRS["adience"],
+    partition="test",
+    transform=DATA_TRANSFORMS["test"],
+)
+
+adience_val_dataset = AdienceDataset(
+    DATA_DIRS["adience"],
+    partition="val",
+    transform=DATA_TRANSFORMS["val"],
+)
+
+# Dataloaders
+# Adience
+adience_train_dataloader = DataLoader(
+    adience_train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
+)
+adience_test_dataloader = DataLoader(
+    adience_test_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
+)
+
+class AdienceMod(pl.LightningDataModule):
+    def __init__(self, root_dir, batch_size, train_tr, val_tr, test_tr):
+        super(AdienceMod, self).__init__()
+        self.root_dir = root_dir
+        self.batch_size = batch_size
+        self.train_tr = train_tr
+        self.val_tr = val_tr
+        self.test_tr = test_tr
+    
+    def setup(self, stage=None):
+        adience_train_dataset = AdienceDataset(
+            self.root_dir, partition="train", transform=self.train_tr
+        )
+        adience_test_dataset = AdienceDataset(
+            self.root_dir, partition="test", transform=self.test_tr
+        )
+        adience_val_dataset = AdienceDataset(
+            self.root_dir, partition="val", transform=self.val_tr
+        )
+        self.train_ds = adience_train_dataset
+        self.val_ds = adience_val_dataset
+        self.test_ds = adience_test_dataset
+
+    def train_dataloader(self):
+        return DataLoader(self.train_ds, batch_size=self.batch_size, shuffle=True, num_workers=0)
+    
+    def val_dataloader(self):
+        return DataLoader(self.val_ds, batch_size=self.batch_size, shuffle=True, num_workers=0)
+
+    def test_dataloader(self):
+        return DataLoader(self.test_ds, batch_size=self.batch_size, shuffle=True, num_workers=0)
 
 class CelebADataset(ImageDataset):
     def __init__(self, root_dir, partition="train", transform=None):
@@ -114,18 +210,6 @@ class CelebADataset(ImageDataset):
 
 # Datasets
 # Adience
-'''adience_train_dataset = AdienceDataset(
-    DATA_DIRS["adience"],
-    train=True,
-    train_ratio=TRAIN_RATIO,
-    transform=DATA_TRANSFORMS["train"],
-)
-adience_test_dataset = AdienceDataset(
-    DATA_DIRS["adience"],
-    train=False,
-    train_ratio=TRAIN_RATIO,
-    transform=DATA_TRANSFORMS["test"],
-)'''
 
 # CelebA
 celeba_train_dataset = CelebADataset(
@@ -138,14 +222,7 @@ celeba_val_dataset = CelebADataset(
     DATA_DIRS["celeba"], partition="val", transform=DATA_TRANSFORMS["val"]
 )
 
-# Dataloaders
-# Adience
-'''adience_train_dataloader = DataLoader(
-    adience_train_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
-)
-adience_test_dataloader = DataLoader(
-    adience_test_dataset, batch_size=BATCH_SIZE, shuffle=True, num_workers=0
-)'''
+
 
 # CelebA
 celeba_train_dataloader = DataLoader(
@@ -189,6 +266,18 @@ class CelebAMod(pl.LightningDataModule):
 
     def test_dataloader(self):
         return DataLoader(self.test_ds, batch_size=self.batch_size, shuffle=True, num_workers=0)
+    
 
+
+'''
+print(len(adience_train_dataset))
+print(len(adience_test_dataset))
+print(len(adience_val_dataset))
+
+sample = adience_train_dataset[0]  # Get the first sample
+image, label = sample
+print(f'Image shape: {image.shape}, Label: {label}')
+'''
 
 celebds = CelebAMod(DATA_DIRS["celeba"], BATCH_SIZE, DATA_TRANSFORMS["train"], DATA_TRANSFORMS["val"], DATA_TRANSFORMS["test"])
+adienceds = AdienceMod(DATA_DIRS["adience"], BATCH_SIZE, DATA_TRANSFORMS["train"], DATA_TRANSFORMS["val"], DATA_TRANSFORMS["test"])
--- train.py
+++ train.py
@@ -1,5 +1,5 @@
-
 from data import celebds
+from data import adienceds
 
 import torch
 from torch import nn
@@ -16,6 +16,7 @@ import matplotlib.pyplot as plt
 from pathlib import Path
 import pandas as pd
 import os
+import numpy as np
 
 from IPython.core.display import display, HTML
 from pytorch_lightning.callbacks import ModelCheckpoint
@@ -24,7 +25,7 @@ from pytorch_lightning.callbacks import ModelCheckpoint
 class LitModel(pl.LightningModule):
     def __init__(
         self,
-        n_classes,
+        n_classes = 2, #created a default value
         download_pretrained=True,
         wandb_logger=None,
         **kwargs,
@@ -35,12 +36,16 @@ class LitModel(pl.LightningModule):
         self.train_accuracy = Accuracy(task='binary')
         self.val_accuracy = Accuracy(task='binary')
         self.test_accuracy = Accuracy(task='binary')
-        self.loss = nn.CrossEntropyLoss()
+        self.loss = nn.BCELoss()
+        '''self.loss = nn.CrossEntropyLoss()'''
         # Used to log images preds to wandb
         self.opt_params = {
             "lr": 0.001,
         }
         self.validation_step_outputs = []
+        self.train_accuracies = []
+        self.train_losses = []
+        self.val_losses = []
 
     def forward(self, x):
         x = self.model(x)
@@ -56,13 +61,16 @@ class LitModel(pl.LightningModule):
         train_acc = self.train_accuracy(y_hat_logits.float(), y.type(torch.FloatTensor).view(y.size(0), 1).to('cuda:0'))
         self.log("train_acc", train_acc, prog_bar=True)
         self.log("train_loss", loss)
+        self.train_accuracies.append(train_acc)
+        self.train_losses.append(loss)
         return {"loss": loss, "progress_bar": {"train_acc": train_acc}}
 
     def validation_step(self, batch, batch_idx):
         x, y = batch
         y_hat_logits = self.forward(x)
         #self.val_accuracy(y_hat_logits, y)
-        val_loss = self.loss(y_hat_logits.float(), y.float().view(y.size(0), 1).to('cuda:0'))
+        val_loss = self.loss(y_hat_logits.float(), y.type(torch.FloatTensor).view(y.size(0), 1).to('cuda:0'))
+        self.val_losses.append(val_loss)
         val_loss.requires_grad = True
         self.validation_step_outputs.append(val_loss)
         return {"val_loss": val_loss, "out_logits": y_hat_logits}
@@ -87,13 +95,67 @@ class LitModel(pl.LightningModule):
         optimizer = torch.optim.Adam(self.parameters(), **self.opt_params)
         return optimizer
     
-
+'''
+def visualize_samples(dataloader, num_samples=5):
+    batch = next(iter(dataloader))
+    images, labels = batch
+    
+    for idx in range(min(num_samples, len(images))):
+        plt.imshow(np.transpose(images[idx].numpy(), (1, 2, 0)))  # Convert CxHxW to HxWxC for visualization
+        plt.title(f'Label: {labels[idx]}')
+        plt.show()
+'''
 model = LitModel(2)
+
+adienceds.setup()
 ckpt = ModelCheckpoint()
+#model = LitModel.load_from_checkpoint('epoch=5-step=244158.ckpt')
+trainer = Trainer(max_epochs=10, callbacks=[ckpt])
+trainer.fit(model, train_dataloaders=adienceds.train_dataloader(), val_dataloaders=adienceds.val_dataloader())
 
-trainer = Trainer(max_epochs=6, callbacks=[ckpt])
+print(len(model.train_losses))
+print(len(model.val_losses))
+print(len(model.train_accuracies))
 
-trainer.fit(model, celebds)
+diff_length = len(model.train_losses) - len(model.val_losses)
+
+# Extend val_losses with NaN values to match the length
+model.val_losses.extend([np.nan] * diff_length)
+
+df = pd.DataFrame({'Training Losses': model.train_losses,
+                   'Training Acccuracies': model.train_accuracies,
+                   'Validation Losses': model.val_losses})
+df.to_csv('losses.csv', index=False)
+
+plt.figure(figsize=(10, 5))
+plt.plot(model.train_losses, label='Training Losses', color='blue')
+plt.plot(model.val_losses, label='Validation Losses', color='red')
+plt.title('Training and Validation Losses')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend()
+plt.grid(True)
+plt.savefig("losses_plot.png")
+plt.show()
+
+model.eval()
+
+predictions = []
+actuals = []
+
+with torch.no_grad():
+    for batch in adienceds.test_dataloader():
+        inputs, labels = batch
+        outputs = model(inputs)
+        predictions.extend(outputs.cpu().numpy())
+        actuals.extend(labels.cpu().numpy())
+
+num_images_to_show = 5
+
+for i in range(num_images_to_show):
+    plt.imshow(np.transpose(adienceds.test_ds[i][0].numpy(), (1, 2, 0)))  # Assuming images are in [C, H, W] format
+    plt.title(f"Actual: {actuals[i]}, Predicted: {predictions[i]}")
+    plt.show()
 
 
 
